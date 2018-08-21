0d9f0b1a4c6201a2a21a4dae6181ad8ccec3317c
Roy Velich <xenomarz@gmail.com>
2018-08-21 03:15:51 +0300

    buid graph implementation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import toxic_comments_classifier
+import tensorflow as tf
 
 glove_model = toxic_comments_classifier.GloveModel()
 glove_model.load_glove_model('.\\glove.6B\\glove.6B.50d.txt')
 
 toxic_comments = toxic_comments_classifier.ToxicComments(glove_model)
-toxic_comments.load_toxic_comments('.\\all\\train.csv', '.\\all\\test.csv', '.\\all\\test_labels.csv')
-_, _ = toxic_comments.get_next_batch(64)
-y = 5
+toxic_comments.load_toxic_comments('.\\all\\train.csv', '.\\all\\test_merged.csv')
 
-
+toxic_comments_rnn = toxic_comments_classifier.ToxicCommentsRNN(toxic_comments=toxic_comments, state_size=64, batch_size=64)
+toxic_comments_rnn.build_graph()
+toxic_comments_rnn.train_graph()
--- toxic_comments_classifier.py
+++ toxic_comments_classifier.py
@@ -79,7 +79,7 @@ class GloveModel:
                 self._token_to_embedding = pickle.load(handle)
         else:
             self.initialize()
-            file = open(glove_model_file_path, encoding="utf8")
+            file = open(glove_model_file_path, encoding="ISO-8859-1")
             _, embedding = self.parse_line(self.peek_line(file))
             self.append_model("<UNK>", np.zeros(embedding.size, dtype=float))
             for line in file:
@@ -123,6 +123,12 @@ class GloveModel:
     def vocabulary_size(self):
         return len(self._embeddings)
 
+    @property
+    def embedding_dimension(self):
+        if not self._embeddings:
+            return 0
+        return len(self._embeddings[0])
+
 
 class ToxicComments:
     def __init__(self, word_embeddings_model):
@@ -133,10 +139,12 @@ class ToxicComments:
         self.size = 0
         self.epochs = 0
         self._toxic_comments_train_data = []
+        self._toxic_comments_test_data = []
 
-    def load_toxic_comments(self, train_data_file_path, test_data_file_path, test_labels_file_path):
+    def load_toxic_comments(self, train_data_file_path, test_data_file_path):
         self.initialize()
         self.load_toxic_comments_train_data(train_data_file_path)
+        self.load_toxic_comments_test_data(test_data_file_path)
         self.size = len(self._toxic_comments_train_data)
         self.shuffle()
 
@@ -145,7 +153,7 @@ class ToxicComments:
             with open('.\\toxic_comments_train_data.pickle', 'rb') as handle:
                 self._toxic_comments_train_data = pickle.load(handle)
         else:
-            with open(train_data_file_path, encoding="utf8") as csv_file:
+            with open(train_data_file_path, encoding="ISO-8859-1") as csv_file:
                 dict_reader = csv.DictReader(csv_file)
                 for csv_row in dict_reader:
                     toxic_comment = ToxicComment(csv_row, self._word_embeddings_model)
@@ -155,6 +163,23 @@ class ToxicComments:
                 pickle.dump(self._toxic_comments_train_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
             return self._toxic_comments_train_data
+
+    def load_toxic_comments_test_data(self, test_data_file_path):
+        if os.path.exists('.\\toxic_comments_test_data.pickle'):
+            with open('.\\toxic_comments_test_data.pickle', 'rb') as handle:
+                self._toxic_comments_test_data = pickle.load(handle)
+        else:
+            with open(test_data_file_path, encoding="ISO-8859-1") as csv_file:
+                dict_reader = csv.DictReader(csv_file)
+                for csv_row in dict_reader:
+                    if csv_row['toxic'] != '-1':
+                        toxic_comment = ToxicComment(csv_row, self._word_embeddings_model)
+                        self._toxic_comments_test_data.append(toxic_comment)
+
+            with open('.\\toxic_comments_test_data.pickle', 'wb') as handle:
+                pickle.dump(self._toxic_comments_test_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+            return self._toxic_comments_test_data
 
     def shuffle(self):
         random.shuffle(self._toxic_comments_train_data)
@@ -168,20 +193,19 @@ class ToxicComments:
         end_range = self.cursor + batch_size
         next_batch_indexed_tokens_tensor = self.get_next_batch_indexed_tokens(start_range, end_range, batch_size)
         next_batch_labels_tensor = self.get_next_batch_labels(start_range, end_range, batch_size)
+        next_batch_sequence_length = self.get_next_batch_sequence_length(start_range, end_range, batch_size)
         self.cursor += batch_size
-        return next_batch_indexed_tokens_tensor, next_batch_labels_tensor
+        return next_batch_indexed_tokens_tensor, next_batch_labels_tensor, next_batch_sequence_length
 
     def get_next_batch_indexed_tokens(self, start_range, end_range, batch_size):
         indexed_tokens_list = []
         for index in range(start_range, end_range):
             indexed_tokens_list.append(self._toxic_comments_train_data[index].indexed_tokens)
-
         indexed_tokens_lengths = [len(indexed_tokens) for indexed_tokens in indexed_tokens_list]
         max_length = max(indexed_tokens_lengths)
         next_batch_indexed_tokens_tensor = np.zeros([batch_size, max_length], dtype=np.int32)
         for i, padded_indexed_tokens in enumerate(next_batch_indexed_tokens_tensor):
             padded_indexed_tokens[:len(indexed_tokens_list[i])] = indexed_tokens_list[i]
-
         return next_batch_indexed_tokens_tensor
 
     def get_next_batch_labels(self, start_range, end_range, batch_size):
@@ -190,77 +214,121 @@ class ToxicComments:
             next_batch_labels_tensor[i] = self._toxic_comments_train_data[i].labels
         return next_batch_labels_tensor
 
+    def get_next_batch_sequence_length(self, start_range, end_range, batch_size):
+        next_batch_sequence_length_tensor = np.zeros([batch_size])
+        for i in range(start_range, end_range):
+            next_batch_sequence_length_tensor[i] = len(self._toxic_comments_train_data[i].indexed_tokens)
+        return next_batch_sequence_length_tensor
+
     @property
     def toxic_comments_train_data(self):
         return self._toxic_comments_train_data
 
     @property
+    def toxic_comments_test_data(self):
+        return self._toxic_comments_test_data
+
+    @property
     def word_embeddings_model(self):
         return self._word_embeddings_model
 
+
 class ToxicCommentsRNN:
-    def __init__(self, toxic_comments):
+    def __init__(self, toxic_comments, state_size, batch_size):
         self._toxic_comments = toxic_comments
+        self._state_size = state_size
+        self._batch_size = batch_size
 
-    # def reset_graph():
-    #     if 'sess' in globals() and sess:
-    #         sess.close()
-    #     tf.reset_default_graph()
+    def build_graph(self):
+        tf.reset_default_graph()
+        vocabulary_size = self._toxic_comments.word_embeddings_model.vocabulary_size
+        embedding_dimension = self._toxic_comments.word_embeddings_model.embedding_dimension
+        embeddings_shape = [vocabulary_size, embedding_dimension]
 
-    def build_rnn(self, vocab_size, state_size = 64, batch_size = 256, num_classes = 6):
-        # reset_graph()
+        # Constants
+        self._keep_prob = tf.constant(1.0)
 
         # Placeholders
-        x = tf.placeholder(tf.int32, [batch_size, None])  # [batch_size, num_steps]
-        seqlen = tf.placeholder(tf.int32, [batch_size])
-        y = tf.placeholder(tf.int32, [batch_size])
-        keep_prob = tf.constant(1.0)
+        self._x = tf.placeholder(tf.int32, [self._batch_size, None])  # [batch_size, num_steps]
+        self._sequence_length = tf.placeholder(tf.int32, [self._batch_size])
+        self._y = tf.placeholder(tf.int32, [self._batch_size, None])
+        self._embeddings_placeholder = tf.placeholder(tf.float32, embeddings_shape)
 
-        # Embedding layer
-        embeddings = tf.get_variable('embedding_matrix', [self._toxic_comments.vocabulary_size, state_size])
-        rnn_inputs = tf.nn.embedding_lookup(embeddings, x)
+        # Embeddings layer
+        self._embeddings_variable = tf.Variable(tf.constant(0.0, shape=embeddings_shape), trainable=False, name="embeddings")
+        self._embeddings_init = self._embeddings_variable.assign(self._embeddings_placeholder)
+
+        # RNN Inputs
+        self._rnn_inputs = tf.nn.embedding_lookup(self._embeddings_variable, self._x)
 
         # RNN
-        cell = tf.nn.rnn_cell.GRUCell(state_size)
-        init_state = tf.get_variable('init_state', [1, state_size], initializer=tf.constant_initializer(0.0))
-        init_state = tf.tile(init_state, [batch_size, 1])
-        rnn_outputs, final_state = tf.nn.dynamic_rnn(cell, rnn_inputs, sequence_length=seqlen, initial_state=init_state)
+        self._cell = tf.nn.rnn_cell.GRUCell(self._state_size)
+        self._init_state = tf.get_variable('init_state', [1, self._state_size], initializer=tf.constant_initializer(0.0))
+        self._init_state = tf.tile(self._init_state, [self._batch_size, 1])
+        self._rnn_outputs, self._final_state = tf.nn.dynamic_rnn(self._cell, self._rnn_inputs, sequence_length=self._sequence_length, initial_state=self._init_state)
 
-        # Add dropout, as the model otherwise quickly overfits
-        rnn_outputs = tf.nn.dropout(rnn_outputs, keep_prob)
+        # Get last RNN output
+        batch_range = tf.range(self._batch_size)
+        indices = tf.stack([batch_range, self._sequence_length - 1], axis=1)
+        self._last_rnn_output = tf.gather_nd(self._rnn_outputs, indices)
 
-        """
-        Obtain the last relevant output. The best approach in the future will be to use:
 
-            last_rnn_output = tf.gather_nd(rnn_outputs, tf.pack([tf.range(batch_size), seqlen-1], axis=1))
-
-        which is the Tensorflow equivalent of numpy's rnn_outputs[range(30), seqlen-1, :], but the
-        gradient for this op has not been implemented as of this writing.
-
-        The below solution works, but throws a UserWarning re: the gradient.
-        """
-        idx = tf.range(batch_size) * tf.shape(rnn_outputs)[1] + (seqlen - 1)
-        last_rnn_output = tf.gather(tf.reshape(rnn_outputs, [-1, state_size]), idx)
-
-        # Softmax layer
-        with tf.variable_scope('softmax'):
-            W = tf.get_variable('W', [state_size, num_classes])
-            b = tf.get_variable('b', [num_classes], initializer=tf.constant_initializer(0.0))
-        logits = tf.matmul(last_rnn_output, W) + b
+        W = tf.get_variable('W_output', [self._state_size, 6])
+        b = tf.get_variable('b_output', [6], initializer=tf.constant_initializer(0.0))
+        logits = tf.matmul(self._last_rnn_output, W) + b
         preds = tf.nn.softmax(logits)
-        correct = tf.equal(tf.cast(tf.argmax(preds, 1), tf.int32), y)
+
+        correct = tf.equal(tf.cast(preds > 0.5, tf.int32), self._y)
         accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
-        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits, y))
+        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=self._y))
         train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
-        return {
-            'x': x,
-            'seqlen': seqlen,
-            'y': y,
-            'dropout': keep_prob,
-            'loss': loss,
-            'ts': train_step,
-            'preds': preds,
-            'accuracy': accuracy
-        }
+    def train_graph(self):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+
+            # indexed_tokens, labels, sequence_length = self._toxic_comments.get_next_batch(64)
+            # feed_dict = {
+            #     self._x: indexed_tokens,
+            #     self._sequence_length: sequence_length,
+            #     self._y: labels,
+            #     self._embeddings_placeholder: self._toxic_comments.word_embeddings_model.embeddings
+            # }
+            #
+            # sess.run(self._embeddings_init, feed_dict=feed_dict)
+            # bla = sess.run(self._rnn_inputs, feed_dict=feed_dict)
+            # bla2 = sess.run(self._rnn_outputs, feed_dict=feed_dict)
+            # bla3 = sess.run(self._last_rnn_output, feed_dict=feed_dict)
+
+            step, accuracy = 0, 0
+            tr_losses, te_losses = [], []
+            current_epoch = 0
+            while current_epoch < num_epochs:
+                step += 1
+                batch = tr.next_batch(batch_size)
+                feed = {g['x']: batch[0], g['y']: batch[1], g['seqlen']: batch[2], g['dropout']: 0.6}
+                accuracy_, _ = sess.run([g['accuracy'], g['ts']], feed_dict=feed)
+                accuracy += accuracy_
+
+                if tr.epochs > current_epoch:
+                    current_epoch += 1
+                    tr_losses.append(accuracy / step)
+                    step, accuracy = 0, 0
+
+                    # eval test set
+                    te_epoch = te.epochs
+                    while te.epochs == te_epoch:
+                        step += 1
+                        batch = te.next_batch(batch_size)
+                        feed = {g['x']: batch[0], g['y']: batch[1], g['seqlen']: batch[2]}
+                        accuracy_ = sess.run([g['accuracy']], feed_dict=feed)[0]
+                        accuracy += accuracy_
+
+                    te_losses.append(accuracy / step)
+                    step, accuracy = 0, 0
+                    print("Accuracy after epoch", current_epoch, " - tr:", tr_losses[-1], "- te:", te_losses[-1])
+
+
+
